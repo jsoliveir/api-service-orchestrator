4c25ed6fab72efed631ad39c384382f9ca42ee1f
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-08-06 23:55:13 +0100

    improvements

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request,Response
+from modules.utils import serializable
 from models.Workflows import Workflow
 import logging
 import json
 import glob
 import os
+
 
 app = Flask(__name__)
 
@@ -18,8 +20,13 @@ async def trigger_http(route):
                 if request.method.lower() in workflow.http.verbs:
                     response.headers["Content-Type"]  = "application/json"
                     result = await workflow.run(request)
-                    response.data = json.dumps(result)
-                    response.status_code = 200
+                    
+                    response.data = json.dumps(serializable({
+                        "name" : workflow.name,
+                        "steps": [s for s in filter(lambda s: not s.hidden or type(s.result) is Exception,workflow.steps)],
+                        "success" : workflow.success
+                    }))
+                    response.status_code = 200 if result.success else 500 
                     return response
         response.data = "<h1> 404 - Workflow not found/h1>"
         response.status_code = 404
